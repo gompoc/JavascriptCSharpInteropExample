8c6039ae54b71a05f2fe7e49699f7c2c54d1723c
gompoc <91314780+gompoc@users.noreply.github.com>
2022-05-01 20:19:22 +0100

    Initial commit

---
+++ Example.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace ExampleProject;
+
+public static class Example
+{
+    [JSInvokable]
+    public static async Task<int> Sum(int x, int y)
+    {
+        return await JavascriptComponent.JSRuntime.InvokeAsync<int>("jsSum", new object?[] {x, y});
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ExampleProject;
+
+
+Console.WriteLine("Application OnInit()");
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<JavascriptComponent>("#app");
+await builder.Build().RunAsync();
+
+
+
